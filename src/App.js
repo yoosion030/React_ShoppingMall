582038c106eb8494c246461b1f2908db47578ea2
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-29 15:27:08 +0900

    Add Content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react';
+import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
 
 function App() {
+
+  
+  let [shoes,shoesRevise] = useState(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Jumbotron></Jumbotron>
+      <div className="container">
+        <div className="row">
+        {
+          shoes.map(function(a,i){
+            return (
+              <Shoes i = {i}></Shoes>
+            );
+          })
+        }
+        </div>
+      </div>
     </div>
   );
+}
+function Jumbotron(){
+  return(
+    <div className="jumbotron">
+      <h1>20% Season Off</h1>
+    </div>
+  );
+}
+function Shoes(props){
+  return(
+    <div className="col-md-4">
+            <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) + '.jpg'} alt="" />
+            <h4>{Data[props.i].title}</h4>
+            <p> {Data[props.i].content} & {Data[props.i].price} </p>
+    </div>
+  )
 }
 
 export default App;
